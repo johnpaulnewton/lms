30d66fa2c2c284b8dda30a421099b5ecbc2c2025
John-Paul Newton <john-paulnewton@john-pauls-air.mynetworksettings.com>
2025-09-28 16:53:08 -0400

    test

--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -4,6 +4,7 @@ import { PrismaClient } from '../../../../packages/database/generated/client';
 @Injectable()
 export class PrismaService extends PrismaClient {
     onModuleInit() {
+        console.log('DATABASE_URL:', process.env.DATABASE_URL);
         this.$connect()
             .then(() => console.log('Connected to DB'))
             .catch((err) => {
