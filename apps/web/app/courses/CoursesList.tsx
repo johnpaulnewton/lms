05c238bad912916bcc32a9e2df3819a282323385
John-Paul Newton <john-paulnewton@john-pauls-air.mynetworksettings.com>
2025-10-05 14:14:24 -0400

    add course page

---
+++ apps/web/app/courses/CoursesList.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { use } from "react";
+import "./CoursesList.css";
+
+export default function CoursesList({
+  enrollments,
+}: {
+  enrollments: Promise<{ term: string; course: { id: number; title: string;} }[]>;
+}) {
+  const data = use(enrollments);
+
+  return (
+    <div className="enrollments-container">
+      <div className="enrollments-header">
+        <span>Course</span>
+        <span>Term</span>
+      </div>
+      <ul className="enrollments-list">
+        {data.map((enrollment) => (
+          <li key={enrollment.course.id} className="enrollment-item">
+          <a href={`/courses/${enrollment.course.id}`} className="course-link">
+            {enrollment.course.title}
+          </a>
+          <span className="term">{enrollment.term}</span>
+        </li>
+        
+        ))}
+      </ul>
+    </div>
+
+  );
+}
--- apps/web/app/courses/page.tsx
+++ apps/web/app/courses/page.tsx
@@ -1,5 +1,26 @@
-export default function Dashboard() {
+import { Suspense } from "react";
+import CoursesList from "./CoursesList";
+
+async function getCourses() {
+    const CURRENT_USER_ID = "cmgd1e16w000018r5gww3qebe"; //placeholder for now
+    const res = await fetch("https://f25-cisc474-individual-2zzz.onrender.com/enrollments/user/cmgd1e16w000018r5gww3qebe");
+
+    if (!res.ok) {
+        throw new Error("Failed to fetch courses");
+    }
+
+    return res.json();
+}
+
+export default function Page() {
+    const enrollments = getCourses();
+
     return (
-                <h1>Courses</h1>
-    );
+    <div>
+      <h1>My Courses</h1>
+      <Suspense fallback={<p>Loading your courses...</p>}>
+        <CoursesList enrollments={enrollments} />
+      </Suspense>
+    </div>
+  );
 }
