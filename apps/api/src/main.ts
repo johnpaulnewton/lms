f6a57a9932f03695cddd91a09b90bd66e9cb9462
John-Paul Newton <john-paulnewton@john-pauls-air.mynetworksettings.com>
2025-10-10 17:48:17 -0400

    fix cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,28 +4,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-// <<<<<<< HEAD
-// =======
   const port = process.env.PORT || 3000;
   const host = process.env.HOST || undefined;
   // Read allowed origins from environment variable
   const origins = process.env.CLIENT_ORIGINS
     ? process.env.CLIENT_ORIGINS.split(',').map((origin) => origin.trim())
-    : ['http://localhost:3000', 'http://localhost:3001'];
+    : ['http://localhost:3000', 'http://localhost:3001', 'https://f25-cisc474-individual-2zzz.onrender.com'];
   app.enableCors({
     origin: origins,
     credentials: true,
   });
   await app.listen(port, host);
 }
-// >>>>>>> upstream/main
 
-  // app.enableCors({
-  //   origin: ['http://localhost:3001', 'https://f25-cisc474-individual-2zzz.onrender.com'],
-  //   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-  // });
-
-  // const port = process.env.PORT;
-  // await app.listen(port);
-  // }
 bootstrap();
