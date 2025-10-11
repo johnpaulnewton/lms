3a00d9686bbabaab4b8271a1fe8d2299893473ba
John-Paul Newton <john-paulnewton@john-pauls-air.mynetworksettings.com>
2025-10-10 22:10:57 -0400

    fix cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,19 +1,23 @@
-import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const port = process.env.PORT || 3000;
+  const host = process.env.HOST || undefined;
+  // Read allowed origins from environment variable
+  const origins = process.env.CLIENT_ORIGINS
+    ? process.env.CLIENT_ORIGINS.split(',').map((origin) => origin.trim())
+    : ['http://localhost:3001',
+       'http://localhost:3002',
+       'https://lms.jpnewton.workers.dev',
+       'https://f25-cisc474-individual-2zzz.onrender.com' 
+      ];
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001', 'https://f25-cisc474-individual-2zzz.onrender.com'],
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    origin: origins,
+    credentials: true,
   });
-
-  const port = process.env.PORT || 3000; 
-  const host = process.env.HOST || '0.0.0.0'; 
-
   await app.listen(port, host);
 }
 
-bootstrap();
+void bootstrap();
